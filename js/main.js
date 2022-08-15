c1240d2437c1495083f863f481817ce90b48cd62
Vinh Le <vinhyuki1995@gmail.com>
2022-08-15 14:56:13 +0800

    first commit coffee Web

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//create header-wrap
+let root = document.querySelector("#root");
+let headerWrap = document.createElement("div");
+headerWrap.classList.add("header-wrap");
+
+root.appendChild(headerWrap);
+//creat header-content
+let headerWrapMenu = document.createElement("header");
+header.classList.add("header-wrap-menu");
